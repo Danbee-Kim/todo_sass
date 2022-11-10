513f329d65dbd2012f9bef8d1e7268f941dbac14
Danbee Kim <kdb2360@gmail.com>
2022-11-10 14:36:09 +0900

    기능 구현

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import './TodoInsert.scss';
 import { MdAdd } from 'react-icons/md';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []); //리렌더링될 때마다 함수를 새로 만드는 것이 아니라, 한번 함수를 만들고 재사용할 수 있도록 유즈콜백 사용
+
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue('');
+      e.preventDefault(); //submit 이벤트는 브라우저에서 새로고침을 발생시키기 때문에 이를 방지하기 위해서 이 함수를 호출
+    },
+    [onInsert, value],
+  );
+
   return (
-    <form className="TodoInsert">
-      <input placeholder="할 일을 입력하세요" />
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={onChange}
+      />
       <button type="submit">
         <MdAdd />
       </button>
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -5,15 +5,17 @@ import {
   MdRemoveCircleOutline,
 } from 'react-icons/md';
 import './TodoListItem.scss';
+import cn from 'classnames'; //조건부로 결합할 수 있는 유틸리티 사용
 
-const TodoListItem = () => {
+const TodoListItem = ({ todo, onRemove, onToggle }) => {
+  const { id, text, checked } = todo;
   return (
     <div className="TodoListItem">
-      <div className="checkbox">
-        <MdCheckBoxOutlineBlank />
-        <div className="text">할 일</div>
+      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
+        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+        <div className="text">{text}</div>
       </div>
-      <div className="remove">
+      <div className="remove" onClick={() => onRemove(id)}>
         <MdRemoveCircleOutline />
       </div>
     </div>
